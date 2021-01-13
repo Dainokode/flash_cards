9e1d05331d2ca407d3e929895823872e16cca103
Antonio <antoniodainok@gmail.com>
2021-01-13 10:17:00 +0100

    final commit

--- main.py
+++ main.py
@@ -6,29 +6,43 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 
-# csv to dataframe
-data = pd.read_csv("data/french_words.csv")
-words_to_learn = data.to_dict(orient="records")
-current_card = {}
+current_cards = {}
+french_words = {}
 
 
-def random_card(button_id):
-    global current_card
-    current_card = random.choice(words_to_learn)
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    french_words = original_data.to_dict(orient="records")
+else:
+    french_words = data.to_dict(orient="records")
+
+
+
+def random_card():
+    global current_cards
+    current_cards = random.choice(french_words)
     canvas.itemconfig(canvas_image, image=card_front)
     canvas.itemconfig(card_title, text="French", fill="black")
-    canvas.itemconfig(card_text, text=current_card["French"], fill="black")
-    if button_id == 2:
-        words_to_learn.remove(current_card)
+    canvas.itemconfig(card_text, text=current_cards["French"], fill="black") 
     window.after(3000, show_translation)
-    print(words_to_learn)
+    print(current_cards)
+
+
+
+def is_known():
+    french_words.remove(current_cards)
+    df = pd.DataFrame(french_words)
+    df.to_csv("data/words_to_learn.csv", index=False)
+    random_card()
 
 
 
 def show_translation():
     canvas.itemconfig(canvas_image, image=card_back)
     canvas.itemconfig(card_title, text="English", fill="white")
-    canvas.itemconfig(card_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_text, text=current_cards["English"], fill="white")
    
 
 
@@ -53,117 +67,17 @@ card_text = canvas.create_text(400, 250, fill="black", font=("arial", 60, "bold"
 
 # buttons
 w_button = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=w_button, highlightthickness=0, command=lambda:random_card(1))
+wrong_button = Button(image=w_button, highlightthickness=0, command=random_card)
 wrong_button.grid(row=2, column=1, pady=15)
 
 
 r_button = PhotoImage(file="./images/right.png")
-right_button = Button(image=r_button, highlightthickness=0, command=lambda:random_card(2))
+right_button = Button(image=r_button, highlightthickness=0, command=is_known)
 right_button.grid(row=2, column=2, pady=15)
 
 
-random_card(3)
+random_card()
 window.after(3000, show_translation)
 
 # run program
 window.mainloop()
-
-
-# seulement,only
-# police,police
-# pensais,thought
-# aide,help
-# demande,request
-# genre,kind
-# mois,month
-# frère,brother
-# laisser,let
-# car,because
-# mettre,to put
-# aucun,no
-# laisse,leash
-# eux,them
-# ville,city
-# chaque,each
-# parlé,speak
-# arrivé,come
-# devrait,should
-# bébé,baby
-# longtemps,long time
-# heures,hours
-# vont,will
-# pendant,while
-# revoir,meet again
-# aucune,any
-# place,square
-# parle,speak
-# compris,understood
-# savais,knew
-# étaient,were
-# attention,Warning
-# voici,here is
-# pourrais,could
-# affaire,case
-# donner,give
-# type,type
-# leurs,their
-# donné,given
-# train,train
-# corps,body
-# endroit,place
-# yeux,eyes
-# façon,way
-# écoute,listen
-# dont,whose
-# trouve,find
-# premier,first
-# perdu,lost
-# main,hand
-# première,first
-# côté,side
-# pouvoir,power
-# vieux,old
-# sois,be
-# tiens,here
-# matin,morning
-# tellement,so much
-# enfant,child
-# point,point
-# venu,came
-# suite,after
-# pardon,sorry
-# venez,come
-# devant,in front of
-# vers,towards
-# minutes,minutes
-# demandé,request
-# chambre,bedroom
-# mis,placed
-# belle,beautiful
-# droit,law
-# aimerais,would like to
-# aujourd'hui,today
-# mari,husband
-# cause,cause
-# enfin,finally
-# espère,hope
-# eau,water
-# attendez,Wait
-# parti,left
-# nouvelle,new
-# boulot,job
-# arrêter,Stop
-# dirait,would say
-# terre,Earth
-# compte,account
-# donne,given
-# loin,far
-# fin,end
-# croire,believe
-# chérie,sweetheart
-# gros,large
-# plutôt,rather
-# aura,will have
-# filles,girls
-# jouer,to play
-# bureau,office
